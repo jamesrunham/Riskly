eb7a98b568044fc33216db6a14db83bcae043f97
James Runham <james.runham@reedonline.co.uk>
2012-07-18 16:39:48 +0100

    changed icon

--- HeatMapWebApi/HeatMapWebApi/Scripts/Home/Index.js
+++ HeatMapWebApi/HeatMapWebApi/Scripts/Home/Index.js
@@ -5,21 +5,18 @@
             value : 100,
             slide: function () {
                 $('#checkinCount').html($(".checkinSlider").slider("option", "value"));
+            },
+            change : function () {
+                $('#checkinCount').html($(".checkinSlider").slider("option", "value"));
             }
         }
     );
+    
     $(".preloader").hide();
     $('#clearButton').hide();
     $(".datepicker").datepicker({ dateFormat: 'dd/mm/yy', showAnim: 'fade', prevText: '<<<', nextText: '>>>' });
     $("#logWrapper").hide();
-
     $('#checkinCount').html($(".checkinSlider").slider("option", "value"));
-    
-
-    $(".checkinSlider").mousedown(function () {
-        var value = $(".checkinSlider").slider("option", "value");
-        console.log(value);
-    });
 
     $('#showMap').click(function () {
         if ($('#infovis').html != null)
